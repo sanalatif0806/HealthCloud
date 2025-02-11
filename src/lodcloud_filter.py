76f3f0dcfa01151a4acc904ff42b77ddbc60ce9f
Gabriele <gabrieletuozzo@gmail.com>
2025-02-11 19:55:51 +0000

    added code to perform the filtering of the LOD Cloud with gpt-4o mini

--- src/lodcloud_filter.py
+++ src/lodcloud_filter.py
@@ -2,6 +2,10 @@ import json
 import requests
 import os
 from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+openai_api_key = os.getenv('OPENAI_API_KEY')
 
 here = os.path.dirname(os.path.abspath(__file__))
 
@@ -60,6 +64,7 @@ class LODCloudFilter:
 
 
     def filter_with_gpt(self):
+        client = OpenAI(api_key=openai_api_key)
         categorize_prompt = '''
             I give you some description and title about dataset in the Linked Open Data Cloud, I have to categorize it as Cultural Heritage or Not.  
             For datasets that are Cultural Heritage, you also need to further specify whether it is Tangible, Intangible, Natural Heritage and finally those that define thesaurus and data models, classify them as Terminology.  
@@ -74,13 +79,12 @@ class LODCloudFilter:
         tasks = []
         for index, kg in enumerate(self.lodcloud_data.keys()):
             kg_metadata = self.lodcloud_data[kg]
-            kg_data_prompt = f"dataset_id: {kg}\nTitle: {kg_metadata.title}\nDescription: {kg_metadata.description.en}"
+            kg_data_prompt = f"dataset_id: {kg}\nTitle: {kg_metadata['title']}\nDescription: {kg_metadata['description']['en']}"
             task = {
                 "custom_id": f"task-{index}",
                 "method": "POST",
                 "url": "/v1/chat/completions",
                 "body": {
-                    # This is what you would have in your Chat Completions API call
                     "model": "gpt-4o-mini",
                     "temperature": 0.1,
                     "response_format": { 
@@ -98,9 +102,71 @@ class LODCloudFilter:
                     ],
                 }
             }
+            tasks.append(task)
+       
+        with open(os.path.join(here,'../data/gpt_tasks.jsonl'), 'w') as file:
+            for obj in tasks:
+                file.write(json.dumps(obj) + '\n')
+        
+        batch_file = client.files.create(
+            file=open(os.path.join(here,'../data/gpt_tasks.jsonl'), "rb"),
+            purpose="batch"
+        )
+
+        print(batch_file)
+        
+        batch_job = client.batches.create(
+            input_file_id=batch_file.id,
+            endpoint="/v1/chat/completions",
+            completion_window="24h"
+        )
+
+        batch_job = client.batches.retrieve(batch_job.id)
+        print(batch_job)
+
+    def retrieve_and_save_job_result(self, job_id):
+        client = OpenAI(api_key=openai_api_key)
+        batch_job = client.batches.retrieve(job_id)
+        result_file_id = batch_job.output_file_id
+        result = client.files.content(result_file_id).content
+
+        result_file_name = os.path.join(here,'../data/batch_job_results.jsonl')
+        with open(result_file_name, 'wb') as file:
+            file.write(result)
+
+        results = []
+        with open(result_file_name, 'r') as file:
+            for line in file:
+                json_object = json.loads(line.strip())
+                results.append(json_object)
+        
+        json_results = []
+        for result in results:
+            json_response = result['response']['body']['choices'][0]['message']['content']
+            json_results.append(json.loads(json_response))
+        
+        with open(os.path.join(here,'../data/gpt_results.json'), "w", encoding="utf-8") as file:
+            json.dump(json_results, file)
+    
+    def from_gpt_response_to_lodcloud_data(self):
+        filtered_kgs = {}
+        gpt_results = json.load(open(os.path.join(here,'../data/gpt_results.json'), "r", encoding="utf-8"))
+        for result in gpt_results:
+            kg_id = result['id']
+            if 'category' in result:
+                kg_metadata = self.lodcloud_data[kg_id]
+                category = result['category']
+                kg_metadata['keywords'].append(category)
+                if 'sub_category' in result:
+                    sub_category = result['sub_category']
+                    kg_metadata['keywords'].append(sub_category)
+                filtered_kgs[kg_id] = kg_metadata
+        
+        self.write_filtered_data(filtered_kgs, "gpt_filtered")
 
 
 l = LODCloudFilter()
-l.filter_by_keywords()
-l.filter_by_title_and_description()
-l.ch_lodcloud_intersection(['../data/CHlodcloud_data_keyword.json','../data/CHlodcloud_data_title_description.json'])
+# l.filter_by_keywords()
+# l.filter_by_title_and_description()
+# l.ch_lodcloud_intersection(['../data/CHlodcloud_data_keyword.json','../data/CHlodcloud_data_title_description.json'])
+l.filter_with_gpt()
