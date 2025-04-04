b312cc476d39566a657f6a5439a3aeaa9557407a
Gabriele <gabrieletuozzo@gmail.com>
2025-04-04 19:51:21 +0000

    added the data transformation to create the cloud

--- WebApp/backend/src/routes/CHe_cloud_data.js
+++ WebApp/backend/src/routes/CHe_cloud_data.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { response } = require('express');
 const { getAllIdsAndLinks } = require('../models/CHe_cloud_data');
 
 router.get('/links', async (req, res) => {
@@ -8,8 +9,26 @@ router.get('/links', async (req, res) => {
         if (!items.length) {
             return res.status(404).json({ message: "No elements founded." });
         }
-
-        res.json(items);
+        const nodes = items.map(item => ({
+            "id": item.identifier,
+            "url": `https://example.com/${item.identifier}`,
+            "category": "Type 1"
+        }));
+        const links = [];
+        items.forEach(item => {
+            item.links.forEach(link => {
+            links.push({
+                "source": item.identifier,
+                "target": link.target,
+                "value": link.value,
+            });
+            });
+        });
+        const response = {
+            "nodes" : nodes,
+            "links" : links
+        }
+        res.json(response);
     } catch (error) {
         res.status(500).json({ message: "Server error" });
     }
