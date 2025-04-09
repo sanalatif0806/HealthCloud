fix: FAIRness calculation

CHe_cloud_generator/src/evaluate_fairness.py
@@ -39,8 +39,10 @@ class EvaluateFAIRness:
         dump_indication = self.quality_data["Availability of RDF dump (metadata)"].apply(lambda x: 1 if x in [1,"1"] else 0)
         verifiability_info = self.quality_data.apply(utils.check_publisher_info,axis=1)
         mediatype_indication = self.quality_data['metadata-media-type'].apply(lambda x: 1 if x not in ('[]','['',]',"['']") else 0)
-        # TODO: Add the check also on MR license, but in the SPARQL endpoint
+
         license = self.quality_data['License machine redeable (metadata)'].apply(lambda x: 1 if x not in ['-', '',False,'False'] and pd.notna(x) else 0)
+        license_query = self.quality_data['License machine redeable (query)'].apply(lambda x: 1 if x not in ['-', '',False,'False','[]'] and pd.notna(x) else 0)
+        license_value = (license | license_query).astype(int)
         vocabs =  self.quality_data['Vocabularies'].apply(lambda x: 1 if x not in ['[]','-'] and pd.notna(x) else 0)
         if not self.manually_picked:
             links = self.quality_data['Degree of connection'].apply(lambda x: 1 if (x != '-' and pd.notna(x) and int(x) > 0 ) else 0)
@@ -51,7 +53,7 @@ class EvaluateFAIRness:
                 axis=1
             )
         void_indication = self.quality_data['Url file VoID'].apply(lambda x: 1 if pd.notna(x) and x != '' else 0)
-        self.fairness_evaluation["F2b-M Metadata availability for all the attributes covered in the FAIR score computation"] = ((sparql_indication + doi_indication + dump_indication + verifiability_info + mediatype_indication + license + vocabs + links + void_indication) / 9).round(2)
+        self.fairness_evaluation["F2b-M Metadata availability for all the attributes covered in the FAIR score computation"] = ((sparql_indication + doi_indication + dump_indication + verifiability_info + mediatype_indication + license_value + vocabs + links + void_indication) / 9).round(2)
 
         self.fairness_evaluation["F3-M Data referrable via a DOI"] = self.quality_data['KG id'].apply(utils.recover_doi_from_lodcloud)
 
@@ -68,10 +70,15 @@ class EvaluateFAIRness:
 
         sparql_availability = self.quality_data["Sparql endpoint"].apply(lambda x: 1 if x == 'Available' else 0)
         dump_availability = self.quality_data["Availability of RDF dump (metadata)"].apply(lambda x: 1 if x in [1,"1"] else 0) # No consideration about the mediatype of the available dummp
-        self.fairness_evaluation["A1-D Working access point(s)"] = (
+        sparql_on_not_interop = self.quality_data["SPARQL endpoint URL"].apply(utils.check_at_least_sparql_on)
+        sparql_or_dump_on = (
             (sparql_availability == 1) | (dump_availability == 1)
         ).astype(int)
 
+        self.fairness_evaluation["A1-D Working access point(s)"] = sparql_or_dump_on.combine(
+            sparql_on_not_interop,
+            lambda x, y: 1 if x == 1 else (0.5 if x == 0 and y == 1 else 0)
+        )
 
         if self.manually_picked:
             sparql_metadata = self.quality_data["SPARQL endpoint URL"].apply(utils.check_meta_in_sparql)
@@ -80,10 +87,11 @@ class EvaluateFAIRness:
         else:
             self.fairness_evaluation["A1-M Metadata availability via working primary sources"] = 1
 
-        
         self.fairness_evaluation["A1.2 Authentication & HTTPS support"] = self.quality_data.apply(
-            lambda row: (0 if row["Use HTTPS"] in [False, 'False'] and 'https' in row['SPARQL endpoint URL'] else 1)  +  (1 if row["Requires authentication"] in ["False", False, True, 'True'] else 0) / 2,
-            axis=1) 
+            lambda row: ((1 if (row["Use HTTPS"] in ['True',True] or row["Sparql endpoint"] == 'Available') else 0) + 
+                        (1 if row["Requires authentication"] in ["False", False, True, 'True'] else 0)) / 2,
+            axis=1
+        )
 
         if not self.manually_picked:
             self.fairness_evaluation["A2-M Registered in search engines"] = 1
@@ -115,8 +123,12 @@ class EvaluateFAIRness:
             axis=1
         )
 
-        self.fairness_evaluation['R1.3-M Metadata are described with VoID/DCAT predicates'] = self.quality_data['SPARQL endpoint URL'].apply(utils.check_meta_in_sparql, axis=1)
-
+        metadata_in_sparql = self.quality_data['SPARQL endpoint URL'].apply(utils.check_meta_in_sparql)
+        self.fairness_evaluation['R1.3-M Metadata are described with VoID/DCAT predicates'] = (
+            (self.quality_data['metadata-media-type'].str.contains('meta/void', na=False).astype(int) | 
+            (metadata_in_sparql == 1)).astype(int) | 
+            (~self.quality_data['License machine redeable (query)'].isin(['-','',False,'False'])).astype(int)
+        )
         self.fairness_evaluation["R score"] = (self.fairness_evaluation[["R1.1 Machine- or human-readable license retrievable via any primary source", "R1.2 Publisher information, such as authors, contributors, publishers, and sources", "R1.3-D Data organized in a standardized way", "R1.3-M Metadata are described with VoID/DCAT predicates"]].sum(axis=1) / 4).round(2)
 
         print("Reusability evaluation completed!")
@@ -128,10 +140,15 @@ class EvaluateFAIRness:
             else (1 if 'api/sparql' or 'rdf' in row['metadata-media-type'].lower() else 0),
             axis=1
         )
+        
+        metadata_in_sparql = self.quality_data['SPARQL endpoint URL'].apply(utils.check_meta_in_sparql)
+        self.fairness_evaluation['I1-M Metadata are described with VoID/DCAT predicates'] = (
+            (self.quality_data['metadata-media-type'].str.contains('meta/void', na=False).astype(int) | 
+            (metadata_in_sparql == 1)).astype(int) | 
+            (~self.quality_data['License machine redeable (query)'].isin(['-','',False,'False'])).astype(int)
+        )
 
-        self.fairness_evaluation['I1-M Metadata are described with VoID/DCAT predicates'] = self.quality_data['SPARQL endpoint URL'].apply(utils.check_meta_in_sparql, axis=1)
-
-        self.fairness_evaluation['I2 Use of FAIR vocabularies'] = self.quality_data['Vocabularies'].apply(utils.check_if_fair_vocabs)
+        self.fairness_evaluation['I2 Use of FAIR vocabularies'] = (self.quality_data['Vocabularies'].apply(utils.check_if_fair_vocabs)).round(2)
 
         if not self.manually_picked:
             self.fairness_evaluation['I3-D Degree of connection'] = self.quality_data['Degree of connection'].apply(lambda x: 1 if (x != '-' and pd.notna(x) and int(x) > 0 ) else 0)
@@ -163,7 +180,7 @@ class EvaluateFAIRness:
         self.fairness_evaluation.to_csv(self.output_file_path,index=False)
     
 
-fairness = EvaluateFAIRness('../data/quality_data/manually_picked.csv','../data/fairness_evaluation/CHe-Cloud_manually_picked.csv')
+fairness = EvaluateFAIRness('../data/quality_data/2025-04-04__LODCloud_manually_refined.csv','../data/fairness_evaluation/CHe-Cloud_manually_refined.csv')
 fairness.evaluate_findability()
 fairness.evaluate_availability()
 fairness.evaluate_interoperability()