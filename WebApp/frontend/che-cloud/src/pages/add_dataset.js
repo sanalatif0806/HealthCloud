d256f40d1baf5b40ab7bfd424f732e1161d6644f
Gabriele <gabrieletuozzo@gmail.com>
2025-04-25 07:18:58 +0000

    fix: add note about filling all fields for better FAIRness score

--- WebApp/frontend/che-cloud/src/pages/add_dataset.js
+++ WebApp/frontend/che-cloud/src/pages/add_dataset.js
@@ -11,6 +11,7 @@ const ContactPage = () => {
         <p className="mb-4 text-muted">
           Please fill out the form below to submit a new dataset to the Cultural Heritage Cloud. This will create a Pull Request in the CHe Cloud GitHub repository, and an operator will check it and merge it if everything is correct.
         </p>
+        <p className="mb-4 text-muted"><span className="fw-bold">N.B:</span> If you fill in all the fields, your dataset will have a better chance of having a higher FAIRness score.</p>
         <FormComponent />
       </section>
     </main>
