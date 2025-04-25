0c0b274b6e26de541a016e8d3797f1fef5fd3a7d
Gabriele <gabrieletuozzo@gmail.com>
2025-04-25 15:37:04 +0000

    feat: add update_CHe_cloud.py for merging cloud data with monitoring requests

---
+++ CHe_cloud_generator/src/update_CHe_cloud.py
@@ -0,0 +1,34 @@
+import os
+import json
+import LOD_Cloudify
+import glob
+
+def create_list_and_move(data_to_convert):
+
+    lodcloud_data_list = []
+    # Create a list of dictionaries
+    for key in data_to_convert:
+        lodcloud_data_list.append(data_to_convert[key])
+
+    absolute_path = os.path.abspath(os.path.join('..','..','WebApp/backend/'))
+    with open(absolute_path + '/CHe_cloud_data.json', 'w') as f:
+        json.dump(lodcloud_data_list, f, indent=4)
+
+def merge_cloud_with_monitoring_requests(path_dataset_to_include_in_the_cloud):
+    monitoring_requests_path = os.path.abspath(os.path.join('..','..','WebApp/monitoring_requests/'))
+
+    json_files = glob.glob(os.path.join(monitoring_requests_path, '*.json'))
+    
+    json_files.append(path_dataset_to_include_in_the_cloud)
+    
+    merged_data = {}
+    for path in json_files:
+        path = os.path.abspath(path)
+        with open(path, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            merged_data.update(data)
+    
+    return merged_data
+
+che_cloud_data = merge_cloud_with_monitoring_requests('../data/only_CH_lodcloud/CHe_cloud_merged.json')
+create_list_and_move(che_cloud_data)
