c8c16082263de7b6dc77863d253e31ef86dc07be
Gabriele <gabrieletuozzo@gmail.com>
2025-05-05 13:46:37 +0000

    feat: add button to navigate to dataset insertion page in Cloud component

--- WebApp/frontend/che-cloud/src/pages/cloud.js
+++ WebApp/frontend/che-cloud/src/pages/cloud.js
@@ -1,15 +1,21 @@
 import Graph from "../components/Graph";
 import { base_url } from '../api';
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Cloud(){
     const [data, setData] = useState({ nodes: [], links: [] });
+    const navigate = useNavigate();
 
     useEffect(() => {
             fetch(`${base_url}/CHe_cloud_data/all_ch_links`)
                 .then(response => response.json())
                 .then(data => setData(data));
     }, []);
+
+    const handleInsertResource = () => {
+        navigate('/add-dataset'); // naviga alla pagina /add-dataset
+    }
 
     return (
         <div>
@@ -25,6 +31,26 @@ function Cloud(){
             }}>
                 CHe Cloud!
             </h1>
+            <div style={{ textAlign: 'left', marginBottom: '1.5rem', marginLeft: '2rem' }}>
+                <button 
+                    onClick={handleInsertResource}
+                    style={{
+                        padding: '0.5rem 1.3rem',
+                        fontSize: '1rem',
+                        backgroundColor: '#8da89f',
+                        color: '#fff',
+                        border: 'none',
+                        borderRadius: '5px',
+                        cursor: 'pointer',
+                        boxShadow: '0 2px 5px rgba(0,0,0,0.2)',
+                        transition: 'background-color 0.3s ease'
+                    }}
+                    onMouseOver={(e) => e.target.style.backgroundColor = '#7b978c'}
+                    onMouseOut={(e) => e.target.style.backgroundColor = '#8da89f'}
+                >
+                    Ask to insert resource
+                </button>
+            </div>
             <Graph data={data}/>
         </div>
     )
