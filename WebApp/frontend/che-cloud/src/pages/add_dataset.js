1293f920874f7a8daba9ef416eb8b09a46434db4
Gabriele <gabrieletuozzo@gmail.com>
2025-05-05 13:46:25 +0000

    feat: add navigation button to insert resource in Cloud component

--- WebApp/frontend/che-cloud/src/pages/add_dataset.js
+++ WebApp/frontend/che-cloud/src/pages/add_dataset.js
@@ -9,7 +9,7 @@ const ContactPage = () => {
           Add a New Dataset into the  <span className="fw-bold">CHe Cloud</span>
         </h1>
         <p className="mb-4 text-muted">
-          Please fill out the form below to submit a new dataset to the Cultural Heritage Cloud. This will create a Pull Request in the CHe Cloud GitHub repository, and an operator will check it and merge it if everything is correct.
+          Please fill out the form below to submit a new dataset to the Cultural Heritage Cloud (CHe Cloud). This will create a Pull Request in the CHe Cloud GitHub repository, and an operator will check it and merge it if everything is correct.
         </p>
         <p className="mb-4 text-muted"><span className="fw-bold">N.B:</span> If you fill in all the fields, your dataset will have a better chance of having a higher FAIRness score.</p>
         <FormComponent />
