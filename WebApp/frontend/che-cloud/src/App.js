f8402ca91cbd0ff930221709ef70624d8d949ea2
Gabriele <gabrieletuozzo@gmail.com>
2025-09-17 13:21:07 +0000

    feat: add Google Analytics integration with react-ga4 for pageview tracking

--- WebApp/frontend/che-cloud/src/App.js
+++ WebApp/frontend/che-cloud/src/App.js
@@ -8,8 +8,16 @@ import AddDataset from './pages/add_dataset';
 import Search from './pages/search';
 import Dashboard from './pages/dashboard';
 import About from './pages/about';
+import ReactGA from 'react-ga4'
+const GA_ID = process.env.REACT_APP_GA_ID
 
 function App() {
+  ReactGA.initialize(GA_ID);
+  
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+  }, []);
+  
   return (
       <Router basename='/'>
         <Routes>
