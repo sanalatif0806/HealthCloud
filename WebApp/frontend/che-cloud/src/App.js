8f49624b04ddd05ed835d65e6f3e0cf9d2562415
Gabriele <gabrieletuozzo@gmail.com>
2025-09-17 13:28:51 +0000

    feat: import React and useEffect in App.js for improved functionality

--- WebApp/frontend/che-cloud/src/App.js
+++ WebApp/frontend/che-cloud/src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
 import Cloud from './pages/cloud';
