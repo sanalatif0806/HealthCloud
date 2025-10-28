271414e81521a029b2627c298aeedba8957e7e87
Gabriele <gabrieletuozzo@gmail.com>
2025-10-28 15:56:05 +0000

    feat: enhance dataset form to handle editing and adding datasets dynamically

--- WebApp/frontend/che-cloud/src/pages/add_dataset.js
+++ WebApp/frontend/che-cloud/src/pages/add_dataset.js
@@ -4,12 +4,17 @@ import { useNavigate } from 'react-router-dom';
 import Footer from '../components/footer';
 import { Link } from 'react-router-dom';
 import Navbar from '../components/navbar';
+import { useLocation } from 'react-router-dom';
 
 const AddDataset = () => {
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const dataset_id = queryParams.get('dataset_id');
   const navigate = useNavigate();
   const handleCheCloud = () => {
     navigate('/'); 
 }
+
   return (
     <>
       <div className="container-fluid mt-3 px-4">
@@ -20,10 +25,20 @@ const AddDataset = () => {
     <main className="container mt-5">
       <section className="bg-light p-5 rounded shadow-sm">
         <h1 className="mb-4 text-success">  
-          Add a New Dataset into the  <span className="fw-bold">CHe Cloud</span>
+          {dataset_id ? (
+            <> Modify the Dataset metadata into the  <span className="fw-bold">CHe Cloud</span></>
+          ) : (
+           <> Add a New Dataset into the  <span className="fw-bold">CHe Cloud</span></>
+          )}
         </h1>
         <p className="mb-4 text-muted">
-          Please fill out the form below to submit a new dataset to the Cultural Heritage Cloud (CHe Cloud). This will create a Pull Request in the CHe Cloud GitHub repository, and an operator will check it and merge it if everything is correct.
+          {
+            dataset_id ? (
+              <> Please modify the dataset metadata below to update the information in the Cultural Heritage Cloud (CHe Cloud). This will create a Pull Request in the CHe Cloud GitHub repository, and an operator will check it and merge it if everything is correct. </>
+            ) : (
+              <> Please fill out the form below to submit a new dataset to the Cultural Heritage Cloud (CHe Cloud). This will create a Pull Request in the CHe Cloud GitHub repository, and an operator will check it and merge it if everything is correct. </>
+            )
+          }
         </p>
         <p className="mb-4 text-muted"><span className="fw-bold">N.B:</span> If you fill in all the fields, your dataset will have a better chance of having a higher FAIRness score.</p>
         <FormComponent />
